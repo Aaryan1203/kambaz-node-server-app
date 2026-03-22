a0f0a0977b06117d0c9ab3ae39a996fd6b8545ef
aaryan1203 <aaryanjain1203@gmail.com>
2026-03-22 16:12:31 -0400

    updated cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: true,
   }),
 );
 const sessionOptions = {
