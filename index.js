c54703527d3b2cf4919936c684670f4c75c18836
aaryan1203 <aaryanjain1203@gmail.com>
2026-03-27 13:24:11 -0400

    a6 start

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ import session from "express-session";
 import ModulesRoutes from "./kambaz/modules/routes.js";
 import AssignmentsRoutes from "./kambaz/assignments/routes.js";
 import EnrollmentsRoutes from "./kambaz/enrollments/routes.js";
+import mongoose from "mongoose";
 
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
